1ea21d29d686ab659e017d4a7257d62bbc1837c1
WillJHaggard <willjhaggard@gmail.com>
2015-10-18 16:55:29 -0400

    Added some basic gulp notes and gutil

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,15 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    gutil = require('gulp-util');
+
+/*
+    Gulp has 4 top level functions:
+        gulp.task - defines your tasks; arguments are (name, deps, fn)
+        gulp.src - points to the files we want to use; params are (globs, optional options object)
+        gulp.dest - points to the output folder we want to write files to
+        gulp.watch - two forms; return an EventEmitter that emits change events; (glob, optional options object, array of tasks)
+*/
+
+// What a gulp task looks like with gutil
+gulp.task('default', function() {
+   return gutil.log('This is the default task.');
+});
